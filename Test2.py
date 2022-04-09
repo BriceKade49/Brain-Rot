e45cc2452c2c7ffd54325f84a3bb240b774022c0
Craft-bit <70213960+Craft-bit@users.noreply.github.com>
2022-04-09 10:16:59 -0400

    test

--- Test2.py
+++ Test2.py
@@ -5,6 +5,9 @@ username = "bdk4949@outlook.com"
 password = "TechStars"
 
 url = "https://www.netflix.com/login"
+# title = ""
+# season = input()
+# episode = input()
 
 driver = webdriver.Chrome("chromedriver.exe")
 
@@ -12,10 +15,10 @@ driver.get(url)
 driver.find_element_by_name("userLoginId").send_keys(username)
 driver.find_element_by_name("password").send_keys(password)
 driver.find_element_by_css_selector(".login-button.btn").click()
-time.sleep(3)
+time.sleep(1)
 driver.find_element_by_class_name('profile-icon').click()
-driver.get("https://www.netflix.com/watch/80133434?trackId=14277283&tctx=-97%2C-97%2C%2C%2C%2C%2C%2C")
-time.sleep(10)
+driver.get("https://www.netflix.com/watch/81218414?source=35")
+time.sleep(1)
 driver.find_element_by_css_selector("button, html input[type=button], input[type=reset], input[type=submit]").click()
 driver.find_element_by_link_text("Play").click()
 
